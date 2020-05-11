22d5089ceca7a37135d68a4256bcdc6e955c9649
Jose Victor Persset <victor.persset@gmail.com>
2020-05-11 17:45:41 -0300

    create a dynamic timeline

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,5 +1,7 @@
 <div class=" border border-blue-400 rounded-lg px-8 py-6 mb-8">
-    <form action="">
+    <form method="POST" action="/tweets">
+    @csrf
+
         <textarea
                         name="body"
                         class="w-full"
@@ -10,12 +12,19 @@
 
         <footer class=" flex justify-between">
             <img 
-                src="https://picsum.photos/40" 
-                alt=""
+                src="{{ auth()->user()->avatar }}" 
+                alt="your avatar"
                 class="rounded-full mr-2"
             >
                     
-            <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
+            <button 
+                type="submit" 
+                class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white"
+            >Tweet-a-roo!</button>
         </footer>              
     </form>
+
+    @error('body')
+        <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
+    @enderror
 </div>
