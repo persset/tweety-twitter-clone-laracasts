ba30e8c60831b4d15c93401c8a450b14c49f20e3
Jose Victor Persset <victor.persset@gmail.com>
2020-05-22 14:18:48 -0300

    file storage and custom avatars

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Validation\Rule;
+
 
 class ProfilesController extends Controller
 {
@@ -13,5 +15,21 @@ class ProfilesController extends Controller
 
     public function edit(User $user) {
         return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user) {
+        $attributes = request()->validate([
+            'username' => ['string', 'required', 'max:255', Rule::unique('users')->ignore($user)],
+            'name' => ['string', 'required', 'max:255'],
+            'avatar' => ['required', 'file'],
+            'email'=> ['string', 'required', 'email', 'max:255', Rule::unique('users')->ignore($user)],
+            'password' => ['string', 'required', 'min:8', 'max:255', 'confirmed'],
+        ]);
+
+        $attributes['avatar'] = request('avatar')->store('avatars');
+
+        $user->update($attributes);
+
+        return redirect($user->path());
     }
 }
--- app/User.php
+++ app/User.php
@@ -15,9 +15,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -37,8 +35,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function getAvatarAttribute() {
-        return "https://i.pravatar.cc/200?u=" . $this->email;
+    public function getAvatarAttribute($value) {
+        return asset('storage/' . $value);
     }
 
     public function timeline() {
@@ -59,7 +57,7 @@ class User extends Authenticatable
     }
 
     public function path($append = '') {
-        $path = route('profile', $this->name);
+        $path = route('profile', $this->username);
 
         return $append ? "{$path}/{$append}" : $path;
     }
